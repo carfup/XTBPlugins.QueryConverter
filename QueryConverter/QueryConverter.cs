7a3c491ddd77dba2555155c2e24ca007a17370e3
Clément O <clement@carfup.com>
2018-10-08 16:05:35 +0200

    handling acewinforms for code syntax !

--- QueryConverter/QueryConverter.cs
+++ QueryConverter/QueryConverter.cs
@@ -17,6 +17,7 @@ using System;
 using Microsoft.Xrm.Sdk.Messages;
 using Carfup.XTBPlugins.AppCode;
 using Microsoft.Crm.Sdk.Messages;
+using AceWinforms;
 
 namespace Carfup.XTBPlugins.QueryConverter
 {
@@ -47,22 +48,122 @@ namespace Carfup.XTBPlugins.QueryConverter
             InitializeComponent();
         }
 
+        private void QueryConverter_Load(object sender, EventArgs e)
+        {
+            inputCodeEditor.Theme = "twilight";
+            inputCodeEditor.HighlighterMode = "csharp";
+            inputCodeEditor.Load();
+
+            outputCodeEditor.Theme = "twilight";
+            outputCodeEditor.HighlighterMode = "csharp";
+            outputCodeEditor.Load();
+        }
+
         private void toolStripButtonCloseTool_Click(object sender, System.EventArgs e)
         {
             CloseTool();
         }
 
-        private void buttonConvert_Click(object sender, EventArgs e)
+        private void buttonConvert_Click(object sender, EventArgs evt)
+        {
+            ExecuteMethod(ProcessToConversion);
+        }
+
+
+        private string GetCodeEditorHighlight(string type)
+        {
+            switch (type.ToLower())
+            {
+                case "queryexpression":
+                    return "csharp";
+                case "fetchxml":
+                    return "xml";
+                case "webapi":
+                    return "curly";
+                default:
+                    return "csharp";
+            }
+        }
+
+        private void comboBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            var data = inputCodeEditor.Text;
+            inputCodeEditor.HighlighterMode = GetCodeEditorHighlight(comboBoxInput.Text);
+            inputCodeEditor.Load();
+            inputCodeEditor.Text = data;
+        }
+
+        private void ProcessToConversion()
         {
             string inputType = comboBoxInput.Text;
             string outputType = comboBoxOutput.Text;
-            string inputQuery = textBoxQueryInput.Text;
+            string inputQuery = inputCodeEditor.Text; //textBoxQueryInput.Text;
 
-            if (converter == null)
-                converter = new ConverterHelper(Service);
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = "Conversion in progress...",
+                Work = (bw, e) =>
+                {
+                    if (converter == null)
+                        converter = new ConverterHelper(Service);
 
-            string outputQuery = converter.ProcessQuery(inputType, outputType, inputQuery);
-            textBoxQueryOutput.Text = outputQuery;
+                    e.Result = converter.ProcessQuery(inputType, outputType, inputQuery);
+                },
+                PostWorkCallBack = e =>
+                {
+                    if (e.Error != null)
+                    {
+                        MessageBox.Show(this, e.Error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    outputCodeEditor.Text = e.Result.ToString();
+                },
+                ProgressChanged = e => { SetWorkingMessage(e.UserState.ToString()); }
+            });
+        }
+
+        private void comboBoxOutput_TextChanged(object sender, EventArgs e)
+        {
+            var data = outputCodeEditor.Text;
+            outputCodeEditor.HighlighterMode = GetCodeEditorHighlight(comboBoxOutput.Text);
+            outputCodeEditor.Load();
+            outputCodeEditor.Text = data;
+        }
+
+        private void comboBoxTheme_TextChanged(object sender, EventArgs evt)
+        {
+            var inputData = inputCodeEditor.Text;
+            var outputData = outputCodeEditor.Text;
+            var theme = "";
+
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = "Modifying the theme...",
+                Work = (bw, e) =>
+                {
+                    Invoke(new Action(() =>
+                    {
+                        inputCodeEditor.Theme = comboBoxTheme.SelectedItem.ToString();
+                        inputCodeEditor.Load();
+
+                        outputCodeEditor.Theme = comboBoxTheme.SelectedItem.ToString();
+                        outputCodeEditor.Load();
+                    }));
+                },
+                PostWorkCallBack = e =>
+                {
+                    if (e.Error != null)
+                    {
+                        MessageBox.Show(this, e.Error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    inputCodeEditor.Text = inputData;
+                    outputCodeEditor.Text = outputData;
+                },
+                ProgressChanged = e => { SetWorkingMessage(e.UserState.ToString()); }
+            });
         }
     }
 }

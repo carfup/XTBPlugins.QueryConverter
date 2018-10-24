addd40968a797b077b3f3c6c80a2cfa2284a5452
Clément O <clement@carfup.com>
2018-10-24 19:06:45 +0200

    init of the unit tests project

---
+++ Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c789aec8-c805-44fd-92bd-fc5b6cad33c0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test/ToWebApi.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Carfup.XTBPlugins.QueryConverter.AppCode;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk;
+using System.Collections.Generic;
+
+namespace Carfup.XTBPlugins.Test
+{
+    [TestClass]
+    public class ToWebApi
+    {
+       ConverterHelper converterHelper = new ConverterHelper(null);
+
+        [TestMethod]
+        public void ShouldGetAllColumns()
+        {
+            var columnSet = new ColumnSet(true);
+
+            Assert.AreEqual(converterHelper.queryExpressionTo.ManageColumsetToWebApi(columnSet), "");
+        }
+
+        [TestMethod]
+        public void ShouldGetMultipleColumns()
+        {
+            var columnSet = new ColumnSet("name", "opportunityid");
+
+            Assert.AreEqual(converterHelper.queryExpressionTo.ManageColumsetToWebApi(columnSet), "$select=name,opportunityid");
+        }
+
+        [TestMethod]
+        public void ShouldGetSingleOrderByAsc()
+        {
+            var orderByAsc = new List<OrderExpression>() {
+                new OrderExpression("name", OrderType.Ascending)
+            };
+
+            Assert.AreEqual(converterHelper.queryExpressionTo.ManageOrdersToWebApi(orderByAsc), "$orderby=name asc");
+        }
+
+        [TestMethod]
+        public void ShouldGetSingleOrderByDesc()
+        {
+            var orderByDesc = new List<OrderExpression>() {
+                new OrderExpression("name", OrderType.Descending)
+            };
+
+            Assert.AreEqual(converterHelper.queryExpressionTo.ManageOrdersToWebApi(orderByDesc), "$orderby=name desc");
+        }
+
+        [TestMethod]
+        public void ShouldGetMultipleOrderByAsc()
+        {
+            var orderByAsc = new List<OrderExpression>() {
+                new OrderExpression("name", OrderType.Ascending),
+                new OrderExpression("opportunityid", OrderType.Ascending),
+            };
+
+            Assert.AreEqual(converterHelper.queryExpressionTo.ManageOrdersToWebApi(orderByAsc), "$orderby=name asc,opportunityid asc");
+        }
+
+        [TestMethod]
+        public void ShouldGetMultipleOrderByDesc()
+        {
+            var orderByDesc = new List<OrderExpression>() {
+                new OrderExpression("name", OrderType.Descending),
+                new OrderExpression("opportunityid", OrderType.Descending)
+            };
+
+            Assert.AreEqual(converterHelper.queryExpressionTo.ManageOrdersToWebApi(orderByDesc), "$orderby=name desc,opportunityid desc");
+        }
+
+        [TestMethod]
+        public void ShouldGetMultipleOrderByMix()
+        {
+            var orderByDesc = new List<OrderExpression>() {
+                new OrderExpression("name", OrderType.Descending),
+                new OrderExpression("opportunityid", OrderType.Ascending)
+            };
+
+            Assert.AreEqual(converterHelper.queryExpressionTo.ManageOrdersToWebApi(orderByDesc), "$orderby=name desc,opportunityid asc");
+        }
+
+        [TestMethod]
+        public void ShouldGetTopCountNull()
+        {
+            Assert.AreEqual(converterHelper.queryExpressionTo.ManageTopCountToWebApi(null), "");
+        }
+
+        [TestMethod]
+        public void ShouldGetTopCount()
+        {
+            Assert.AreEqual(converterHelper.queryExpressionTo.ManageTopCountToWebApi(5), "$top=5");
+        }
+
+        [TestMethod]
+        public void ShouldGetSingleCondition()
+        {
+            var criteria = new FilterExpression
+            {
+                Conditions =
+                {
+                    new ConditionExpression("name", ConditionOperator.Equal, "test"),
+                }
+            };
+
+            Assert.AreEqual(converterHelper.queryExpressionTo.ManageConditionsToWebApi(criteria, ""), "$filter=(name eq 'test')");
+        }
+
+        [TestMethod]
+        public void ShouldGetMultipleConditions()
+        {
+            var criteria = new FilterExpression
+            {
+                Conditions =
+                {
+                    new ConditionExpression("name", ConditionOperator.Equal, "test"),
+                    new ConditionExpression("opportunityid", ConditionOperator.Equal, "guid")
+                }
+            };
+
+            Assert.AreEqual(converterHelper.queryExpressionTo.ManageConditionsToWebApi(criteria, ""), "$filter=(name eq 'test' and opportunityid eq 'guid')");
+        }
+
+        [TestMethod]
+        public void ShouldGetMultipleAndConditions()
+        {
+            var criteria = new FilterExpression
+            {
+                Conditions =
+                {
+                    new ConditionExpression("name", ConditionOperator.Equal, "test"),
+                    new ConditionExpression("opportunityid", ConditionOperator.Equal, "guid")
+                },
+                FilterOperator = LogicalOperator.And
+            };
+
+            Assert.AreEqual(converterHelper.queryExpressionTo.ManageConditionsToWebApi(criteria, ""), "$filter=(name eq 'test' and opportunityid eq 'guid')");
+        }
+
+        [TestMethod]
+        public void ShouldGetMultipleOrConditions()
+        {
+            var criteria = new FilterExpression
+            {
+                Conditions =
+                {
+                    new ConditionExpression("name", ConditionOperator.Equal, "test"),
+                    new ConditionExpression("opportunityid", ConditionOperator.Equal, "guid"),
+                },
+                FilterOperator = LogicalOperator.Or
+            };
+
+            Assert.AreEqual(converterHelper.queryExpressionTo.ManageConditionsToWebApi(criteria, ""), "$filter=(name eq 'test' or opportunityid eq 'guid')");
+        }
+
+        [TestMethod]
+        public void ShouldGetMultipleAndOrConditions()
+        {
+            QueryExpression query = new QueryExpression()
+            {
+                EntityName = "contact",
+                Distinct = false,
+                ColumnSet = new ColumnSet("fullname", "address1_telephone1"),
+                Criteria = {
+                    Filters = {
+                        new FilterExpression {
+                            FilterOperator = LogicalOperator.And,
+                            Conditions = {
+                                new ConditionExpression("name", ConditionOperator.Equal, "test"),
+                                new ConditionExpression("opportunityid", ConditionOperator.Equal, "guid"),
+                            }
+                        },
+                        new FilterExpression {
+                            FilterOperator = LogicalOperator.Or,
+                            Conditions = {
+                                new ConditionExpression("name", ConditionOperator.Equal, "test"),
+                                new ConditionExpression("opportunityid", ConditionOperator.Equal, "guid"),
+                            }
+                        }
+                    }
+                }
+            };
+
+            Assert.AreEqual(converterHelper.queryExpressionTo.ManageConditionsToWebApi(query.Criteria, ""), "$filter=(name eq 'test' and opportunityid eq 'guid') and (name eq 'test' or opportunityid eq 'guid')");
+        }
+    }
+}
